7e921bae4cc0660351ee09b08c456e067e41a32b
Luis Eduardo San Martin <lsanmartin@uchicago.edu>
2020-03-08 21:11:26 -0500

    corrected position where svg objects are rendered

--- src/app.js
+++ src/app.js
@@ -25,14 +25,14 @@
 
 
 
-function area_graph(dataset,max_val,subtitle) {
+function area_graph(dataset, max_val, subtitle, div_section) {
 // set the dimensions and margins of the graph
   var margin = {top: 60, right: 230, bottom: 50, left: 50},
       width = 800 - margin.left - margin.right,
       height = 450 - margin.top - margin.bottom;
 
   // append the svg object to the body of the page
-  var svg = d3.select("#area_chart")
+  var svg = d3.select("#".concat(div_section))
     .append("svg")
       .attr("width", width + margin.left + margin.right)
       .attr("height", height + margin.top + margin.bottom)
@@ -265,13 +265,13 @@ function area_graph(dataset,max_val,subtitle) {
  });
 }
 
-function scatterPlot(datafile){
+function scatterPlot(datafile, div_section){
   var margin = { top: 80, right: 200, bottom: 60, left: 200};
         var width = 1400 - margin.left - margin.right;
         var height = 600 - margin.top - margin.bottom;
 
 
-    var svg = d3.select("body")
+    var svg = d3.select("#".concat(div_section))
       .append("svg")
         .attr("width", width + margin.left + margin.right)
         .attr("height", height + margin.top + margin.bottom)
